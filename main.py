ade4096397864e70fecef8394a036626ca941c3a
The Drew <andrewschom@live.com>
2014-06-14 14:32:23 +0930

    vector system

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
 import pyglet
 import engine
 
+#TODO: Use dt in update.
+
 window = pyglet.window.Window(640, 480)
-colours = engine.Colours()
 
 imgtest = pyglet.image.load("debug.png")
-player = engine.Player(imgtest, 0.098, colours.red)
-tiles = [engine.Block(50,50,colours.red,imgtest), engine.Block(50,100,colours.red,imgtest)]
-paint = [engine.Paint(200,200,colours.blue,imgtest,imgtest)]
-keys = pyglet.window.key.KeyStateHandler()
-window.push_handlers(keys)
+player = pyglet.sprite.Sprite(imgtest, 80, 80)
+mvm = engine.Vector(0,3)
 
 @window.event
 def on_draw():
     window.clear()
-    for tile in tiles:
-        tile.on_draw()
-    player.on_draw()
-    for pain in paint:
-        pain.on_draw()
-
-@window.event
-def on_key_press(key, mods):
-    player.on_key_press(key)
-
-@window.event
-def on_key_release(key, mods):
-    player.on_key_release(key)
+    player.draw()
 
 def update(dt):
-    player.on_update(tiles, paint)
+    player.x, player.y =  mvm.add_to_point(player.x, player.y)
 
 pyglet.clock.schedule_interval(update,1/60)
 
